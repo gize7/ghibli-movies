fd823da346b7f355fe2fbd9f41997cf99ab95004
gize7 <gize@avian.aero>
2022-04-14 06:39:51 +0300

    Added my project

---
+++ app/root.tsx
@@ -0,0 +1,37 @@
+import styles from "./tailwind.css";
+import { LinksFunction, MetaFunction } from "@remix-run/react/routeModules";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Studio Ghibli",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
