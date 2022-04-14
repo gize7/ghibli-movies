f0e838db0bd1d814b01c875d1096e7c906140c9b
gize7 <gize@avian.aero>
2022-04-14 08:47:39 +0300

    ...

--- app/data/comments.ts
+++ app/data/comments.ts
@@ -4,16 +4,19 @@ export type CommentEntry = {
   filmId: string;
 };
 
-export async function getComments(filmId: string) {
-  const url = new URL(window.location.toString());
-  const response = await fetch(`${url?.href}/comments?filmId=${filmId}`);
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3001"
+    : "https://ghibli-movies-rosy.vercel.app";
 
+export async function getComments(filmId: string) {
+  const response = await fetch(`${url}/comments?filmId=${filmId}`);
   return response.json();
 }
 
 export async function addComment(comment: CommentEntry) {
   const url = new URL(window.location.toString());
-  const response = await fetch(`${url?.href}/comments`, {
+  const response = await fetch(`${url}/comments`, {
     method: "POST",
     body: JSON.stringify(comment),
     headers: {
